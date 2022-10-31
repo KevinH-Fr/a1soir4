64a702638cbe885383c2de172677ed7cecccf507
KevinH-Fr <kevin.hoffman.france@gmail.com>
2022-10-31 23:48:17 +0100

    avancement form dashboard

--- app/controllers/accueil_admin_controller.rb
+++ app/controllers/accueil_admin_controller.rb
@@ -3,6 +3,8 @@ class AccueilAdminController < ApplicationController
     def index
       @clients = Client.all
       @produits = Produit.all
+      @commandes = Commande.all
+
 
       @q = Client.ransack(params[:q])
       @clients = @q.result(distinct: true)
