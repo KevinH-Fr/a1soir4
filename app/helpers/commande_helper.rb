9f68d0a0d4c9cdeac12a7cf13a3693c44fb42e26
KevinH-Fr <kevin.hoffman.france@gmail.com>
2022-11-19 21:29:21 +0100

    deduction avoir remb et placement calculs dans helper commande

---
+++ app/helpers/commande_helper.rb
@@ -0,0 +1,27 @@
+module CommandeHelper
+    def du_prix(commande)
+        duPrix = Article.commande_courante(@commande).sum_articles + 
+        Article.joins(:sousarticles).commande_courante(@commande).sum_sousarticles
+    end
+    def du_caution(commande)
+        duCaution = Article.commande_courante(@commande).sum_caution
+    end 
+    def recu_prix(commande)
+        recuPrix = Paiement.commande_courante(@commande).prix_only.sum_paiements
+    end 
+    def recu_caution(commande)
+        recuCaution = Paiement.commande_courante(@commande).caution_only.sum_paiements
+    end 
+
+    def avoirremb_deduit(commande)
+        avoirrembDeduit = Avoirremb.commande_courante(@commande).sum_avoirrembs
+    end 
+
+    def solde_prix(commande)
+        soldePrix = du_prix(commande) - recu_prix(commande) - avoirremb_deduit(commande)
+    end
+    def solde_caution(commande)
+        soldeCaution = du_caution(commande) - recu_caution(commande)
+    end
+
+end 
--- app/models/avoirremb.rb
+++ app/models/avoirremb.rb
@@ -6,4 +6,11 @@ class Avoirremb < ApplicationRecord
 
   scope :commande_courante, ->  (commande_courante) { where("commande_id = ?", commande_courante)}
   
+  scope :avoir_only, -> { where("typeavoirremb = ?", "Avoir")}
+  scope :remboursement_only, -> { where("typeavoirremb = ?", "Remboursement")}
+
+  scope :sum_avoirrembs, -> {sum('montant')}
+  scope :compte_avoirrembs, -> {count('montant')}
+
+
 end
