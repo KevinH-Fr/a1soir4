7f4919bd1015f65e76ac52955349eac0063b5cfd
KevinH-Fr <kevin.hoffman.france@gmail.com>
2023-01-04 16:55:27 +0100

    modif navbar

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,21 +1,20 @@
 class ProfilesController < ApplicationController
   before_action :set_profile, only: %i[ show edit update destroy ]
 
-  # GET /profiles or /profiles.json
   def index
     @profiles = Profile.all
   end
 
-  # GET /profiles/1 or /profiles/1.json
+
   def show
   end
 
-  # GET /profiles/new
+
   def new
     @profile = Profile.new
   end
 
-  # GET /profiles/1/edit
+
   def edit
 
     respond_to do |format|
@@ -28,7 +27,6 @@ class ProfilesController < ApplicationController
 
   end
 
-  # POST /profiles or /profiles.json
   def create
     @profile = Profile.new(profile_params)
 
